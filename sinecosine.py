41c4058a1174e41540e6904fe5be42469ca6aac5
Eric Dennison <ericd@netdenizen.com>
2015-09-12 22:25:20 -0400

    Update sinecosine.py

--- sinecosine.py
+++ sinecosine.py
@@ -5,7 +5,16 @@ Credit: <list sources used, if any>
 
 Assignment:
 
+In this assignment you must use *list comprehensions* to generate sprites that show the behavior
+of certain mathematical functions: sine and cosine. 
 
+The sine and cosine functions are provided in the Python math library. These functions are used
+to relate *angles* to *rectangular* (x,y) coordinate systems and can be very useful in computer
+game design.
+
+Unlike the last assignment using ggame`, this one will not provide any "skeleton" code to fill
+in. You should use your submission for the Picture assignment 
+(https://github.com/HHS-IntroProgramming/Picture) as a reference for starting this assignment. 
 
 See:
 https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/TUTORIAL:-Displaying-Graphics
