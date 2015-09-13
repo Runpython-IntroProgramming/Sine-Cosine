1d3b4cfb1ca70f5fef7fc17a171111dfa7ea2fb7
Eric Dennison <ericd@netdenizen.com>
2015-09-12 21:48:41 -0400

    Create sinecosine.py

---
+++ sinecosine.py
@@ -0,0 +1,27 @@
+"""
+sinecosine.py
+Author: <your name here>
+Credit: <list sources used, if any>
+
+Assignment:
+
+Use the ggame library to "paint" a graphical picture of something (e.g. a house, a face or landscape).
+
+Use at least:
+1. Three different Color objects.
+2. Ten different Sprite objects.
+3. One (or more) RectangleAsset objects.
+4. One (or more) CircleAsset objects.
+5. One (or more) EllipseAsset objects.
+6. One (or more) PolygonAsset objects.
+
+See:
+https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/TUTORIAL:-Displaying-Graphics
+for general information on how to use ggame.
+
+https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/TUTORIAL:-Programmed-Graphics
+for general information on using list comprehensions to generate graphics.
+
+http://brythonserver.github.io/ggame/
+for detailed information on ggame.
+"""
