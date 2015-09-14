59f6bb1a2d6b03980399cfddd93fd7527adb5202
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-14 14:33:45 -0400

    Updated from Brython Server: September 14, 2015 2:33:46 PM GMT-4

--- sinecosine.py
+++ sinecosine.py
@@ -42,7 +42,7 @@ black = Color(0x000000, 1.0)
 thinline = LineStyle(1, black)
 mycircle = CircleAsset(5, thinline, blue)
 #xcoordinates = range(100, 600, 10)
-xcoordinates = range(100+100*math.sin(math.radians(x)))
+xcoordinates = range(100+100*math.asin(math.radians(x)))
 # Generate a list of sprites that form a line!
 sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
 
