994939dd12c95003866848ef750efaab4941c59b
APikielny <apikielny@gmail.com>
2015-09-14 14:34:19 -0400

    Updated from Brython Server: September 14, 2015 2:34:19 PM EDT

--- sinecosine.py
+++ sinecosine.py
@@ -32,3 +32,24 @@ for detailed information on ggame.
 
 import math
 from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, blue)
+myredcircle = CircleAsset(5, thinline, red)
+xcoordinates = range(0, 360, 10)
+
+# Generate a list of sprites that form a line!
+sprites = [Sprite(mycircle, (x, 100+100*math.asin(math.radians(x)))) for x in xcoordinates]
+sprites = [Sprite(myredcircle, (x, 100+100*math.acos(math.radians(x)))) for x in xcoordinates]
+
+
+myapp = App()
+myapp.run()
