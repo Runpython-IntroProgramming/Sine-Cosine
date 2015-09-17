3e3baa9ea4a7c0f3c85add45ea48fabf4f4055e6
jasminelou <yajielou@hanovernorwichschools.org>
2015-09-17 14:47:38 -0400

    Updated from Brython Server: 9/17/2015 2:47:38 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Jasmine Lou
+Credit: my classmates and the internet
 
 Assignment:
 
@@ -30,3 +30,22 @@ http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
 
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+
+from math import sin, cos, radians
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+
+thinline = LineStyle(0.2, black)
+mycircle = CircleAsset(2, thinline, blue)
+xcoordinates = range(100, 360, 10)
+
+sprites = [Sprite(mycircle, (100+100*sin(radians(x)))) for x in xcoordinates]
+
+
+myapp = App()
+myapp.run()
