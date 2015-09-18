ca4d20f09586360057994d492bd26011a3ce2682
ryankynor <ryankynor@hanovernorwichschools.org>
2015-09-18 14:15:30 -0400

    Updated from Brython Server: September 18, 2015 2:15:30 PM EDT

--- sinecosine.py
+++ sinecosine.py
@@ -30,3 +30,17 @@ http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
 
+
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+
+
+
+myapp = App()
+myapp.run()
