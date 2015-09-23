6f53295446bdbfdd6788422bcc04e04726993017
ryankynor <ryankynor@hanovernorwichschools.org>
2015-09-23 13:57:52 -0400

    Updated from Brython Server: 9/23/2015 1:57:52 PM

--- sinecosine.py
+++ sinecosine.py
@@ -36,9 +36,9 @@ from ggame import App, Color, LineStyle, Sprite, CircleAsset
 from math import sin, cos, radians
 
 
-red = Color(0xff0000, 0.5)
+red = Color(0xff0000, 0.15)
 green = Color(0x00ff00, 1.0)
-blue = Color(0x0000ff, 0.5)
+blue = Color(0x0000ff, 0.15)
 black = Color(0x000000, 1.0)
 purple = Color(0x551A8B, 0.25)
 
@@ -46,12 +46,12 @@ purple = Color(0x551A8B, 0.25)
 thinline = LineStyle(1, black)
 
 
-bluecircle = CircleAsset(200, thinline, blue)
-redcircle = CircleAsset(200, thinline, red)
-purplecircle = CircleAsset(200, thinline, purple)
+bluecircle = CircleAsset(50, thinline, blue)
+redcircle = CircleAsset(50, thinline, red)
+purplecircle = CircleAsset(50, thinline, purple)
 
 
-x = range(0, 361, 10)
+x = range(0, 361, 5)
 
 
 [Sprite(bluecircle, (x, 100+100*sin(radians(x)))) for x in range(0, 361, 10)]
