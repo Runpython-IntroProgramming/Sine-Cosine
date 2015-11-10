09fe26e6152daa29bc895ae6a026f6776a2dba40
voidJeff <qy120838@126.com>
2015-11-10 14:18:46 -0500

    Updated from Brython Server: November 10, 2015 2:18:45 PM EST

--- sinecosine.py
+++ sinecosine.py
@@ -43,7 +43,7 @@ purple = Color(0xff00ff, 1.0)
 thinline = LineStyle(0, black)
 
 xcord = range(0, 720, 1)
-dot1 = CircleAsset(1, thinline, red)
+dot1 = RectangleAsset(1,1, thinline, red)
 dot2 = CircleAsset(1, thinline, blue)
 dot3 = CircleAsset(1, thinline, purple)
 xaxis = RectangleAsset(720,1, thinline, black)
