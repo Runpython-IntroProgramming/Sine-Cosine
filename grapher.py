9a07aa29bd3165db6e0c2ff98809ed0969c393a8
voidJeff <qy120838@126.com>
2015-11-18 14:08:46 -0500

    Updated from Brython Server: November 18, 2015 2:08:46 PM EST

--- grapher.py
+++ grapher.py
@@ -35,10 +35,11 @@ class Dot(Sprite):
         
         
     def step(self):
+        print(self.x)
         self.x += 1
         if self.x < 1500:
-            sprite0 = Sprite(Dot.asset, (self.x, 450-k*(self.x-750)-100*b))
-           
+            #sprite0 = Sprite(Dot.asset, (self.x, 450-k*(self.x-750)-100*b))
+            sprite = Sprite(Dot.asset, (self.x, 200))
 class Grapher(App):
     def __init__(self, width, height):
         super().__init__(width, height)
@@ -50,11 +51,12 @@ class Grapher(App):
         sprite1 = Sprite(xaxis, (0, 450))
         sprite2 = Sprite(yaxis, (750, 0))
         
-        Dot((0, 450-k*(0-750)-100*b))
+        Dot((0, 100))
         
         #sprite0 = [Sprite(Dot.asset, (x, 450-k*(x-750)-100*b)) for x in xcord]
         
-    def step():
+    def step(self):
+        print("Heelo")
         for dot in self.getSpritesbyClass(Dot):
             dot.step()
             
