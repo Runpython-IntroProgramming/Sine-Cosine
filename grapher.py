7e13a199d65537aca65cc60842559a27bcfaacee
voidJeff <qy120838@126.com>
2015-11-20 11:37:38 -0500

    Updated from Brython Server: 11/20/2015 11:37:37 AM

--- grapher.py
+++ grapher.py
@@ -6,8 +6,8 @@ SCREEN_WIDTH = 1536
 SCREEN_HEIGHT = 1024
 
 red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
+green = Color(0x00ff00, 1.0)
 black = Color(0x000000, 1.0)
 purple = Color(0xff00ff, 1.0)
 thinline = LineStyle(0, black)
