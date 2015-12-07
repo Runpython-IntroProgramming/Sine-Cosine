edfe245f67541365f928434a056d2724b449e674
voidJeff <qy120838@126.com>
2015-12-07 14:32:05 -0500

    Updated from Brython Server: December 7, 2015 2:32:05 PM EST

--- grapher.py
+++ grapher.py
@@ -112,14 +112,14 @@ class Dot(Sprite):
                     sprite = Sprite(Dot.asset, (self.x, self.y))
                     print("1")
                     
-                if n > 2:
+                if n > 1:
                     self.y -= 1
                     sprite1 = Sprite(Dot.asset, (self.x, self.y))
                     self.loop = True
                     n -= 1
                     print("2")
                     
-                elif n < -2:
+                elif n < -1:
                     self.y += 1
                     sprite1 = Sprite(Dot.asset, (self.x, self.y))
                     self.loop = True
