85125626a62f3614135af49ec4472a0b8b87eed3
voidJeff <qy120838@126.com>
2015-12-22 14:31:42 -0500

    Updated from Brython Server: December 22, 2015 2:31:42 PM EST

--- grapher.py
+++ grapher.py
@@ -52,6 +52,7 @@ class Dot(Sprite):
             elif a < 0:
                 self.x = -100*(-b-math.sqrt(b**2-4*a*c-23.2*a))/(2*a)
         self.loop = False
+        self.visible = False
         
     
     def l(self):
