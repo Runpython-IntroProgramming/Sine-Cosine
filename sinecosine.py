190b48dfbc2821db40a25e503f715802fffb3cdf
averywallis <averybwallis@gmail.com>
2016-01-29 14:46:00 -0500

    Updated from Brython Server: 1/29/2016 2:46:00 PM

--- sinecosine.py
+++ sinecosine.py
@@ -43,7 +43,7 @@ xcoordinates = range(0, 360, 10)
 # Generate a list of sprites that form a line!
 sprites = [Sprite(mycircle, (x, 100+100*sin(radians(x)))) for x in xcoordinates]
 sprites = [Sprite(mycircle1, (x, 100+100*cos(radians(x)))) for x in xcoordinates]
-sprites = [Sprite(mycircle2, (100+100*cos(radians(x)), 400+100*sin(radians(x))))]
+sprites = [Sprite(mycircle2, (100+100*cos(radians(x)), 400+100*sin(radians(x)))) for x in xcoordinates]
 
 
 myapp = App()
