a2f27b024145e1a346749b4dd047651f67df82a7
mcfey <maryfeyrer@hanovernorwichschools.org>
2016-01-29 14:54:07 -0500

    Updated from Brython Server: 1/29/2016 2:54:22 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Mary Feyrer
+Credit: colorpicker.com, Tess Snyder
 
 Assignment:
 
@@ -29,4 +29,27 @@ for general information on using list comprehensions to generate graphics.
 http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+from math import sin, cos, radians
+
+red = Color(0xff0000, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+purple = Color(0x8E2EC9, 1.0)
+
+
+circle1 = (5, thinline, blue)
+circle2 = (5, thinline, red)
+circle3 = (5, thinline, purple)
+xcoordinates = range(0, 360, 10)
+
+sprites = [Sprite(circle1, (x, 100+100*sin(radians(x)))) for x in xcoordinates]
+sprites = [Sprite(circle2, (x, 100+100*cos(radians(x)))) for x in xcoordinates]
+sprites = [Sprite(circle3, (100+100*cos(radians(x)), 400+100*sin(radians(x)))) for x in xcoordinates]
+
+myapp = App()
+myapp.run()
+
+
 
