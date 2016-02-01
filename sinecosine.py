7f7064a2e46a497ecb868bd69cd9261967f7e1ff
phstearns <phstearns13@gmail.com>
2016-02-01 14:08:34 -0500

    Updated from Brython Server: 2/1/2016 2:08:34 PM

--- sinecosine.py
+++ sinecosine.py
@@ -46,8 +46,7 @@ radianc = CircleAsset(5, noline, purple)
 
 sineg = [Sprite(sine, (x, 100+100*math.sin(math.radians(x)))) for x in xcoordinates]
 cosineg = [Sprite(cosine, (x, 100+100*math.cos(math.radians(x)))) for x in xcoordinates]
-radiancg = [Sprite(radianc, (100+100*math.cos(math.radians(x)), 
-    (400+100*math.sin(math.radians(x)))) for x in xcoordinates]
+radiancg = [Sprite(radianc, (100+100*math.cos(math.radians(x)), (400+100*math.sin(math.radians(x))))) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
