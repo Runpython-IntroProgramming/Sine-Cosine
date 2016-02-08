e9d2b825f8e1f36ad01ec11302e7951b13ee1d5b
Hagin Onyango <hagincubes@gmail.com>
2016-02-08 14:58:37 -0500

    Updated from Brython Server: February 8, 2016 2:58:37 PM EST

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: hAg!n 0nYAnG0
+Credit: Glen Passow
 
 Assignment:
 
@@ -29,4 +29,17 @@ for general information on using list comprehensions to generate graphics.
 http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
-
+from ggame import App, Color, LineStyle, Sprite,RectangleAsset, CircleAsset, EllipseAsset
+ 
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+yellow = Color(0xFFFF00, 1.0)
+ 
+ thinline = LineStyle(1,black)
+ 
+ rectangle = RectangleAsset
+ 
+ myapp = App()
+ myapp.run()
