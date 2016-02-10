1b321a796da0ba37501914968568d68cca55ef51
Hagin Onyango <hagincubes@gmail.com>
2016-02-10 14:00:31 -0500

    Updated from Brython Server: February 10, 2016 2:00:30 PM EST

--- sinecosine.py
+++ sinecosine.py
@@ -37,9 +37,11 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 yellow = Color(0xFFFF00, 1.0)
  
- thinline = LineStyle(1,black)
+thinline = LineStyle(1,black)
+
+rectangle = RectangleAsset(50, 20, thinline, blue)
+circle = CircleAsset(50, thinline, yellow)
+triangle = PolygonAsset(50, thinline
  
- rectangle = RectangleAsset
- 
- myapp = App()
- myapp.run()
+myapp = App()
+myapp.run()
