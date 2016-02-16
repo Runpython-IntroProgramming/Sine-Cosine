b400db92166445573520f27e8e4d5240e170ebc2
Hagin Onyango <hagincubes@gmail.com>
2016-02-16 08:43:35 -0500

    Update sinecosine.py

--- sinecosine.py
+++ sinecosine.py
@@ -29,10 +29,10 @@ for general information on using list comprehensions to generate graphics.
 http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
-import ggame 
+#import ggame 
 from ggame import App, Color, LineStyle, Sprite, CircleAsset
 import math
-from math import sin, cos, radians
+#from math import sin, cos, radians
 
 red = Color(0xff0000, 1.0)
 blue = Color(0x0000ff, 1.0)
@@ -41,20 +41,20 @@ thinlineR = LineStyle(1, red)
 thinlineB = LineStyle(1, blue)
 thinlineP = LineStyle(1, purple)
 
-circleA = CircleAsset(5, thinline, red)
-cirbleB = CircleAsset(5, thinline, blue)
-circleC = CircleAsset(5, thinline, purple)
+circleA = CircleAsset(5, thinlineR, red)
+circleB = CircleAsset(5, thinlineB, blue)
+circleC = CircleAsset(5, thinlineP, purple)
 
-list(range(0, 360, 10))
-[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 
-150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 
-280, 290, 300, 310, 320, 330, 340, 350, 360]
+#list(range(0, 360, 10))
+#[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 
+#150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 
+#280, 290, 300, 310, 320, 330, 340, 350, 360]
 
 xcoordinates = range(0, 360, 10)
 # Generate a list of sprites that form a line!
-spriteA = [Sprite(circleA, (x, 100+100*sin(radians(x))) for x in xcoordinates] 
-spriteB = [Sprite(circleB, (x, 100+100*cos(radians(x))) for x in xcoordinates]
-spriteC = [Sprite(circleC, (100+100*cos(radians(x)), 400+100*sin(radians(x)))) for x in xcoordinates]
+spriteA = [Sprite(circleA, (x, 100+100*math.sin(math.radians(x)))) for x in xcoordinates] 
+spriteB = [Sprite(circleB, (x, 100+100*math.cos(math.radians(x)))) for x in xcoordinates]
+spriteC = [Sprite(circleC, (100+100*math.cos(math.radians(x)), 400+100*math.sin(math.radians(x)))) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
