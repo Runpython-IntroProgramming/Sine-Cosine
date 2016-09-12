3f91600cc17c697f781a5c7c4d77ca62304afff8
TheBigBlueBlob <liamszczepiorkowski@hanovernorwichschools.org>
2016-09-12 14:00:53 -0400

    Updated from Brython Server: 9/12/2016 2:00:53 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Liam S
+Credit: None
 
 Assignment:
 
@@ -31,18 +31,26 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite
 from ggame import CircleAsset
+from math import sin, cos, radians
 
 red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
+purple = Color(0x9900ff, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
-mycircle = CircleAsset(5, thinline, blue)
-xcoordinates = range(100, 600, 10)
+mycircle = CircleAsset(5, thinline, red)
+mycircle2 = CircleAsset(5, thinline, blue)
+mycircle3 = CircleAsset(5, thinline, purple)
+xcoordinates = range(0, 360, 10)
+xcoordinates2 = range(0, 360, 10)
+xcoord = lambda x:(100+100*cos(radians(x)))
+xcoordinates3 = range(0, 360, 10)
+xcoordinates4 = [xcoord for x in xcoordinates3]
 
 # Generate a list of sprites that form a line!
-sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
-
+sprites = [Sprite(mycircle, (x, 100+100*sin(radians(x)))) for x in xcoordinates]
+sprites = [Sprite(mycircle2, (x, 100+100*cos(radians(x)))) for x in xcoordinates2]
+sprites = [Sprite(mycircle3, (x, 400+100*sin(radians(x)))) for x in xcoordinates4]
 myapp = App()
 myapp.run()
