f30f79f05fe16150d7e6cb92d80a273ccc9a0812
liama482 <liamabbate@hanovernorwichschools.org>
2016-09-14 13:17:10 -0400

    Updated from Brython Server: 9/14/2016 1:17:09 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,6 +1,6 @@
 """
 sinecosine.py
-Author: <your name here>
+Author: Liam A
 Credit: <list sources used, if any>
 
 Assignment:
@@ -19,10 +19,8 @@ in. You should use your submission for the Picture assignment
 See:
 https://github.com/HHS-IntroProgramming/Sine-Cosine/blob/master/README.md
 for a detailed list of requirements for this assignment.
-
 https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/Displaying-Graphics
 for general information on how to use ggame.
-
 https://github.com/HHS-IntroProgramming/Standards-and-Syllabus/wiki/Programmed-Graphics
 for general information on using list comprehensions to generate graphics.
 
@@ -30,3 +28,33 @@ http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
 
+from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, EllipseAsset, PolygonAsset
+from math import sin, cos, radians
+
+Lgreen = Color (0x7CFC00, 0.95)
+turqo = Color (0x40E0D0, 0.99)
+Orange = Color (0xFF8600, 1)
+purp = Color (0x68228B, 0.6)
+brn = Color (0x5C3317, 0.9)
+pale = Color (0xFFFACD, 0.4)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+crimson = Color(0xA02422, 0.97000000000000000000000000000000000000000000000000000000000000000000000001)
+
+thinline = LineStyle(1, black)
+noline = LineStyle(0.1, blue)
+mycircle = CircleAsset(5, thinline, blue)
+urcircle = CircleAsset(6, thinline, crimson)
+arcircle = CircleAsset(6, thinline, purp)
+rb_xcoord = range(100, 600, 10)
+#gen_xcoor
+
+sprites = [Sprite(mycircle, (x, 100+100*sin(radians(x)))) for x in rb_xcoord]
+sprites = [Sprite(urcircle, (x, 100+100*cos(radians(x)))) for x in rb_xcoord]
+#sprites = [Sprite(arcircle, (x, x*0.6 + 20)) for x in gen_xcoor]
+
+
+
+myapp = App()
+myapp.run()
