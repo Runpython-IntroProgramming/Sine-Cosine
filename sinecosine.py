6fb7b17a350797d4eebb1cb7e4b98acf7d51da42
kezarberger <kezarberger@hanovernorwichschools.org>
2016-09-21 12:58:00 -0400

    Updated from Brython Server: 9/21/2016 12:58:00 PM

--- sinecosine.py
+++ sinecosine.py
@@ -31,6 +31,9 @@ for detailed information on ggame.
 """
 from ggame import App, Color, LineStyle, Sprite
 from ggame import CircleAsset
+from math import sin, cos, radians
+
+
 
 red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
@@ -38,11 +41,13 @@ blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
-mycircle = CircleAsset(5, thinline, blue)
+mycircle = CircleAsset(100, thinline, red)
+mycircle1 = CircleAsset(50
 xcoordinates = range(100, 600, 10)
 
 # Generate a list of sprites that form a line!
 sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
+sprites = [Sprite(mycircle1, (x, x*0.7 + 200)) for x in xcoordinates]
 
 
 myapp = App()
