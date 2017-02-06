571654b69e45b5b079b1b3d89ee667bbece55e4c
pythonisfornormies <kaidarrow@hanovernorwichschools.org>
2017-02-06 14:19:34 -0500

    Updated from Brython Server: 2/6/2017 2:19:34 PM

--- sinecosine.py
+++ sinecosine.py
@@ -59,7 +59,7 @@ circle = CircleAsset(5, thinline, purple)
 
 
 xcoordinates = range(0, 360, 10)
-juoksentelisinkohan = [100+100*cos(radians(x)) for x in range(0, 360, 10)]
+juoksentelisinkohan = [100+100*cos(radians(x)) for x in xcoordinates]
 
 # Generate a list of sprites that form a line!
 sprites = [Sprite(sine, (x, 100 + 100*sin(radians(x)))) for x in xcoordinates]
