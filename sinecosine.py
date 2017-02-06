a62e9ab4dd9130c837a0fbbec2475c0d1f20b0d4
JadedCoder712 <thomaspostans@hanovernorwichschools.org>
2017-02-06 14:51:16 -0500

    Updated from Brython Server: 2/6/2017 2:51:16 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Kyle Postans
+Credit: Mr. Teacherman
 
 Assignment:
 
@@ -35,7 +35,6 @@ from ggame import App, Color, LineStyle, Sprite, RectangleAsset, CircleAsset, El
 from math import sin, cos, radians
 
 red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
 purple = Color(800080, 1.0)
