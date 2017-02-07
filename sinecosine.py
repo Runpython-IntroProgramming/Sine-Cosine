75a8f25dc055619eeb6ae66410795c93440b69e4
Earljb <earlbarrowes@hanovernorwichschools.org>
2017-02-07 14:27:51 -0500

    Updated from Brython Server: 2/7/2017 2:27:51 PM

--- sinecosine.py
+++ sinecosine.py
@@ -39,11 +39,31 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+purple = Color(800080, 1.0)
 
-list(range(0,360,10))
-[x//1 for x in range(0,360,10)]
+list(range(0, 360, 10))
+[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 
+150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 
+280, 290, 300, 310, 320, 330, 340, 350]
+[x//1 for x in range(0, 360, 10)]
 
-sprites = [Sprite(mycircle, (x, 100+100*sin(radians(x)))) for x in xcoordinates]
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, blue)
+xcoordinates = range(0, 360, 10)
+
+sprites = [Sprite(mycircle, (x+100, 200+(-100*sin(radians(x))))) for x in xcoordinates]
+
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, red)
+xcoordinates = range(0, 360, 10)
+
+sprites = [Sprite(mycircle, (x+100, 200+(-100*cos(radians(x))))) for x in xcoordinates]
+
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, purple)
+xcoordinates = range(0, 360, 10)
+
+sprites = [Sprite(mycircle, (500+(-100*cos(radians(x))), 500+(-100*sin(radians(x))))) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
