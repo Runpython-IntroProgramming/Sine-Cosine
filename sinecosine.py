9279d43ef460323eafdc7d2de144828225ca8b1b
NatSimon <nathalieosimon@gmail.com>
2017-02-16 14:48:30 -0500

    Updated from Brython Server: February 16, 2017 2:48:29 PM EST

--- sinecosine.py
+++ sinecosine.py
@@ -37,10 +37,16 @@ from math import sin,cos,radians
 
 red = Color(0xff0000, 1.0)
 blue = Color(0x0000ff, 1.0)
+list(range(0, 360, 10))
+[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 
+150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 
+280, 290, 300, 310, 320, 330, 340, 350]
+thinline = LineStyle(1, black)
 
 myapp = App()
 myapp.run()
 mycircle = CircleAsset(5, thinline, blue)
 xcoordinates = range(100, 600, 10)
+sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
 100+100*sin(radians(x))
 
