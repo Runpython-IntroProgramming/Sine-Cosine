a1b0237ffb9d0b66411a88672c8d23b54c9bb4d4
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-07 13:00:03 -0400

    Updated from Brython Server: 9/7/2018 1:00:02 PM

--- sinecosine.py
+++ sinecosine.py
@@ -30,7 +30,7 @@ http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
 
-from ggame import CircleAsset
+from ggame import CircleAsset, Color, Sprite, App
 import math
 
 black = Color(0x000000, 1.0)
