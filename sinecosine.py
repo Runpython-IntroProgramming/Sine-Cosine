b9bc1c40bb4e42095b452681a9671283c50e95ae
NedrowNomae <42844326+NedrowNomae@users.noreply.github.com>
2018-09-10 12:37:53 -0400

    Updated from Brython Server: 9/10/2018 12:37:52 PM

--- sinecosine.py
+++ sinecosine.py
@@ -40,10 +40,7 @@ yaxis = RectangleAsset(1, 250, thinline, black)
 Sprite((yaxis), (250,0))
 xaxis = yaxis = RectangleAsset(500, 1, thinline, black)
 Sprite((xaxis), (0,125))
-n = 36
-k = 0
 xlist = list(range(0, 360, 10))
-#pi = 4.0*sum([((-1.0)**k)/(2*k+1) for k in range(0,n)])
 y = 100+100*sin(radians(xlist))
 point = CircleAsset(1, thinline, black)
 Sprite((point), (x,y))
