5096c36137157560ae220c04bc50b8fe266a2136
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-09-14 12:39:30 -0400

    Updated from Brython Server: 9/14/2018 12:39:30 PM

--- sinecosine.py
+++ sinecosine.py
@@ -37,13 +37,13 @@ from ggame import App
 blue = Color(0x0000ff, 1.0)
 red = Color(0xff0000, 1.0)
 purple = Color(0x800080, 1.0)
-by10 = [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 280, 290, 300, 310, 320, 330, 340, 350, 360, 370, 380, 390, 400, 410, 420, 430, 440, 450, 460, 470, 480, 490, 500, 510, 520, 530, 540, 550, 560, 570, 580, 590, 600, 610, 620, 630, 640, 650, 660, 670, 680, 690, 700, 710, 720, 730, 740, 750, 760, 770, 780, 790, 800, 810, 820, 830, 840, 850, 860, 870, 880, 890, 900, 910, 920, 930, 940, 950, 960, 970, 980, 990, 1000]
+by10 = range(1,1000)
 thinliner = LineStyle(1, red)
 thinlineb = LineStyle(1, blue)
 thinlinep = LineStyle(1, purple)
 circleb = CircleAsset(13, thinlineb, blue)
 circler = CircleAsset(13, thinliner,red)
-circlep = CircleAsset(7, thinlinep, purple)
+circlep = CircleAsset(13, thinlinep, purple)
 ycoordb = [100+100*sin(radians(x)) for x in by10]
 ycoordr = [100+100*cos(radians(x)) for x in by10]
 xcoordp = [100+100*cos(radians(x)) for x in by10]
