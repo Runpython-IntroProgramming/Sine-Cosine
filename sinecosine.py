3f35414d6e327b4e6ae0221b90535e8bdc8f05b8
jordanvtskier12 <42844401+jordanvtskier12@users.noreply.github.com>
2018-09-25 12:49:30 -0400

    Updated from Brython Server: 9/25/2018 12:49:29 PM

--- sinecosine.py
+++ sinecosine.py
@@ -37,13 +37,24 @@ red = Color(0xff0000, 1.0)
 green = Color(0x00ff00, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+purple=Color(0x00ffff, 1.0)
 
 thinline = LineStyle(1, black)
 mycircle = CircleAsset(5, thinline, blue)
 xcoordinates = range(0, 360, 10)
 
+mycircle2 = CircleAsset(5, thinline, red)
+xcoordinates = range(0, 360, 10)
+
+mycircle3 = CircleAsset(5, thinline, purple)
+xcoordinates = range(0, 360, 10)
+
 # Generate a list of sprites that form a line!
 sprites = [Sprite(mycircle, (x, 100+100*sin(radians(x)))) for x in xcoordinates]
+
+sprites = [Sprite(mycircle2, (x, 270+100*cos(radians(x)))) for x in xcoordinates]
+
+sprites = [Sprite(mycircle3, (400+100*cos(radians(x)),x)) for x in xcoordinates]
 
 myapp = App()
 myapp.run()
