4bf7c2a37888a053c6b3eec3ff22a278ade09640
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-01-30 13:53:03 -0500

    Updated from Brython Server: 1/30/2019 1:53:01 PM

--- sinecosine.py
+++ sinecosine.py
@@ -40,11 +40,15 @@ black = Color(0x000000, 1.0)
 
 thinline = LineStyle(1, black)
 mycircle = CircleAsset(5, thinline, blue)
-xcoordinates = range(100, 600, 10)
+yee = range(0, 360, 10)
+ybluecoordinates = (100+100*sin(radians(yee)))
+yredcoordinates = (100+100*cos(radians(yee)))
+ypurplecoordinates = 400+100*sin(radians(yee))
+xpurplecoordinates = 100+100*cos(radians(yee))
 
-sprites = [Sprite(mycircle, (x, x*0.5 + 100)) for x in xcoordinates]
-
-
+bluee = [Sprite(mycircle, (x, ybluecoordinates)) for x in yee]
+reed = [Sprite(mycircle, (x, x*0.5 + 100)) for x in yee]
+puurple = [Sprite(mycircle, (x, x*0.5 + 100)) for x in yee]
 
 myapp = App()
 myapp.run()
