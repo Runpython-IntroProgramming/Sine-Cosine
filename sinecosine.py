28673d86d132770869b771b0c45102fc8bc4aac3
waSclthu11 <46973645+waSclthu11@users.noreply.github.com>
2019-01-31 12:19:12 -0500

    Updated from Brython Server: 1/31/2019 12:19:12 PM

--- sinecosine.py
+++ sinecosine.py
@@ -37,25 +37,26 @@ red = Color(0xff0000, 1.0)
 purple = Color(0x800080, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
-
+a = 360
 thinline = LineStyle(1, black)
 bluecircle = CircleAsset(5, thinline, blue)
 purplecircle = CircleAsset(5, thinline, purple)
 redcircle = CircleAsset(5, thinline, red)
-xval = range(0, 360, 10)
+xval = range(0, a, 10)
 
-ybluecoordinates = [(100 + 100*sin(radians(x))) for x in range(0, 360, 10)]
+
+ybluecoordinates = [(100 + 100*sin(radians(x))) for x in range(0, a, 10)]
 #ybluecoordinates = 100+100*sin(radians(xval))
-yredcoordinates = [(100+100*cos(radians(x))) for x in range(0, 360, 10)]
+yredcoordinates = [(100+100*cos(radians(x))) for x in range(0, a, 10)]
 #yredcoordinates = 100+100*cos(radians(xval))
-ypurplecoordinates = [(400+100*sin(radians(x))) for x in range(0, 360, 10)]
+ypurplecoordinates = [(400+100*sin(radians(x))) for x in range(0, a, 10)]
 #ypurplecoordinates = 400+100*sin(radians(xval))
-xpurplecoordinates = [(100+100*cos(radians(x))) for x in range(0, 360, 10)]
+xpurplecoordinates = [(100+100*cos(radians(x))) for x in range(0, a, 10)]
 #xpurplecoordinates = 100+100*cos(radians(xval))
 
-bluee = [Sprite(bluecircle, (x, (100 + 100*sin(radians(x))))) for x in range(0, 360, 10) ] 
-reed = [Sprite(redcircle, (x, (100+100*cos(radians(x)))))  for x in range(0, 360, 10)] 
-puurple = [Sprite(purplecircle, ((100+100*cos(radians(x))), (400+100*sin(radians(x))))) for x in range(0, 360, 10)]
+bluee = [Sprite(bluecircle, (x, (100 + 100*sin(radians(x))))) for x in range(0, a, 10) ] 
+reed = [Sprite(redcircle, (x, (100+100*cos(radians(x)))))  for x in range(0, a, 10)] 
+puurple = [Sprite(purplecircle, ((100+100*cos(radians(x))), (400+100*sin(radians(x))))) for x in range(0, a, 10)]
 
 myapp = App()
 myapp.run()
