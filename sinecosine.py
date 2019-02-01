787374f626fed38a13067247d60f0ed749a9ed5a
maBottnn14 Hhs <46973490+MaBottnn14@users.noreply.github.com>
2019-02-01 14:43:01 -0500

    Updated from Brython Server: 2/1/2019 2:43:00 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: maBottn14
+Credit: http://www.discoveryplayground.com/computer-programming-for-kids/rgb-colors/
 
 Assignment:
 
@@ -29,4 +29,30 @@ for general information on using list comprehensions to generate graphics.
 http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+from math import sin, cos, radians
+
+red = Color(0xff0000, 1.0)
+green = Color(0x00ff00, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0x000000, 1.0)
+purple = Color(0x9370db, 1.0)
+
+thinline = LineStyle(1, black)
+circle = CircleAsset(5, thinline, blue)
+xcoordinates = list(range(0, 360, 10))
+[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 
+150, 160, 170, 180, 190, 200, 210, 220, 230, 240, 250, 260, 270, 
+280, 290, 300, 310, 320, 330, 340, 350]
+sprites = [Sprite(circle, (x, 100+100*sin(radians(x)))) for x in xcoordinates]
+
+circle = CircleAsset(5, thinline, red)
+sprites = [Sprite(circle, (x, 100+100*cos(radians(x)))) for x in xcoordinates]
+
+circle = circle = CircleAsset(5, thinline, purple)
+sprites = [Sprite(circle, (100+100*cos(radians(x)), 400+100*sin(radians(x)))) for x in xcoordinates]
+
+myapp = App()
+myapp.run()
 
