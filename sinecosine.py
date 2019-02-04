48098b2397b6450c6120e1a720603774536adb94
kyDoleuc04 <46973452+kyDoleuc04@users.noreply.github.com>
2019-02-04 14:54:41 -0500

    Updated from Brython Server: 2/4/2019 2:54:42 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: kyDoleuc04
+Credit: Mr. Dennison, tutorials
 
 Assignment:
 
@@ -32,8 +32,26 @@ for detailed information on ggame.
 
 from ggame import App, Color, LineStyle, Sprite
 from ggame import CircleAsset
+from math import sin,cos,radians
 
 red = Color(0xff0000, 1.0)
-green = Color(0x00ff00, 1.0)
+purple = Color(0x5B0480, 1.0)
 blue = Color(0x0000ff, 1.0)
 black = Color(0x000000, 1.0)
+
+thinline = LineStyle(1, black)
+mycircle = CircleAsset(5, thinline, blue)
+redcircle = CircleAsset(5, thinline, red)
+purplecircle = CircleAsset(5, thinline, purple)
+xcoords = range(0,360,10)
+xcoords1 = [100+100*cos(radians(x)) for x in xcoords]
+ycoords1 = [400+100*sin(radians(x)) for x in xcoords]
+xycoords = zip(xcoords1,ycoords1)
+
+sprites = [Sprite(mycircle, (x,(100+100*sin(radians(x))))) for x in xcoords]
+sprites1 = [Sprite(redcircle, (x,(100+100*cos(radians(x))))) for x in xcoords]
+sprites2 = [Sprite(purplecircle, (x,y)) for x,y in xycoords]
+
+
+myapp = App()
+myapp.run()
