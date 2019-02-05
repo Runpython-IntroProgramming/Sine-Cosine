e126a858d79789f450c30a1046ee739183314c0d
raLiinu30 <46948627+raLiinu30@users.noreply.github.com>
2019-02-05 13:54:02 -0500

    Updated from Brython Server: 2/5/2019 1:53:59 PM

--- sinecosine.py
+++ sinecosine.py
@@ -1,7 +1,7 @@
 """
 sinecosine.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Rain
+Credit: https://www.w3schools.com/colors/colors_picker.asp, 
 
 Assignment:
 
@@ -29,4 +29,28 @@ for general information on using list comprehensions to generate graphics.
 http://brythonserver.github.io/ggame/
 for detailed information on ggame.
 """
+from ggame import App, Color, LineStyle, Sprite
+from ggame import CircleAsset
+from math import sin, cos, radians 
+
+red = Color(0xff0000, 1.0)
+blue = Color(0x0000ff, 1.0)
+purple = Color(0x6600cc, 1.0)
+black = Color(0x000000, 1.0)
+
+thinline = LineStyle(1, black)
+
+redcircle = CircleAsset(5, thinline, red)
+bluecircle = CircleAsset(5, thinline, blue)
+purplecircle = CircleAsset(5, thinline, purple)
+
+xcoordinates1= range(100, 360, 10)
+xcoordinates2 = range(100, 360, 10)
+
+# Generate a list of sprites that form a line!
+redcircle = [Sprite(redcircle, (x, 100+100*cos(radians(x)))) for x in xcoordinates1] 
+bluecircle = [Sprite(bluecircle, (x, 100+100*sin(radians(x)))) for x in xcoordinates1] 
+
+myapp = App()
+myapp.run()
 
